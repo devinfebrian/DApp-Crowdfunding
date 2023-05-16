51b40204ba195b7017274597cd4aa87e8f4c0832
Devin Febrian <devinfebrian20@gmail.com>
2023-05-16 20:58:56 +0800

    crowdfunding

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Sidebar } from './sidebar';
+export { default as Navbar } from './navbar';
+export { default as CustomButton } from './custombutton';
+export { default as FormField } from './formfield';
+export { default as DisplayCampaigns } from './DisplayCampaigns';
+export { default as FundCard } from './FundCard';
+export { default as CountBox } from './CountBox';
+export { default as Loader } from './Loader';
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './home';
+export { default as Profile } from './profile';
+export { default as CreateCampaign } from './createcampaign';
+export { default as CampaignDetails } from './campaigndetails';
